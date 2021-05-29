cfdd12facbb8ddc274866c175a09300c52fbe7ef
Mufed Tapany <mufedtapany@gmail.com>
2021-05-30 00:28:06 +0200

    Connect Four - jQuery and JavaScript (ES6) solutions

---
+++ index.js
@@ -0,0 +1,107 @@
+/* jQuery Code */
+
+var $board = $(".connect-four");
+var $columns = $board.find(".column");
+var $positions = $board.find(".position");
+var currentPlayer = 1;
+var diags = [
+    [2, 9, 16, 23],
+    [1, 8, 15, 22, 29],
+    [0, 7, 14, 21, 28, 35],
+    [6, 13, 20, 27, 34, 41],
+    [12, 19, 26, 33, 40],
+    [18, 25, 32, 39],
+    [23, 28, 33, 38],
+    [17, 22, 27, 32, 37],
+    [11, 16, 21, 26, 31, 36],
+    [5, 10, 15, 20, 25, 30],
+    [4, 9, 14, 19, 24],
+    [3, 8, 13, 28],
+];
+
+$columns.on("click", function (event) {
+    const $playerTurn = document.getElementById("player-turn");
+    var $column = $(this);
+    var $columnPositions = $column.find(".position");
+
+    var rowIndex = getRowIndex($columnPositions);
+    $columnPositions.eq(rowIndex).addClass("player-" + currentPlayer);
+    if (rowIndex < 0) {
+        return;
+    }
+
+    // check the column
+    checkWinner($columnPositions);
+    // check the row
+    var $rowPositions = getRowPositions(rowIndex);
+
+    checkWinner($rowPositions);
+    getDiagos();
+    console.log("Player", currentPlayer);
+    switchPlayer($playerTurn);
+});
+
+function getDiagos() {
+    diags.forEach(function (diagIndex) {
+        var $diag = $();
+        for (var i = 0; i < diagIndex.length; i++) {
+            var currentDiagIndex = diagIndex[i];
+            $diag = $diag.add($positions.eq(currentDiagIndex));
+        }
+        checkWinner($diag);
+    });
+}
+
+function getRowPositions(rowIndex) {
+    // create empty jquery collection
+    var $row = $();
+
+    for (var i = 0; i < $columns.length; i++) {
+        var $currentColumn = $columns.eq(i);
+        var $columnPositions = $currentColumn.find(".position");
+        var $position = $columnPositions.eq(rowIndex);
+        $row = $row.add($position);
+    }
+    return $row;
+}
+
+function checkWinner($positions) {
+    var count = 0;
+    for (var i = 0; i < $positions.length; i++) {
+        var $current = $positions.eq(i);
+        if ($current.hasClass("player-" + currentPlayer)) {
+            count++;
+            if (count == 4) {
+                alert("player-" + currentPlayer + " is the winner");
+                location.reload();
+            }
+        } else {
+            count = 0;
+        }
+    }
+}
+
+function getRowIndex(columnPositions) {
+    for (var i = columnPositions.length - 1; i >= 0; i--) {
+        var $currentColumn = columnPositions.eq(i);
+        var isTaken =
+            $currentColumn.hasClass("player-1") ||
+            $currentColumn.hasClass("player-2");
+
+        if (!isTaken) {
+            return i;
+        }
+    }
+    console.log("-1");
+    return -1;
+}
+
+function switchPlayer($playerTurn) {
+    if (currentPlayer === 1) {
+        currentPlayer = 2;
+        $playerTurn.innerText = "Your turn: " + currentPlayer;
+    } else {
+        currentPlayer = 1;
+        $playerTurn.innerText = "Your turn: " + currentPlayer;
+    }
+}
---
+++ script.js
@@ -0,0 +1,107 @@
+/* JavaScript Code */
+
+(function () {
+    const board = document.getElementById("connect-four");
+    const columns = board.querySelectorAll(".column");
+    const positions = board.querySelectorAll(".position");
+    let currentPlayerNumber = 1;
+    const diags = [
+        [2, 9, 16, 23],
+        [1, 8, 15, 22, 29],
+        [0, 7, 14, 21, 28, 35],
+        [6, 13, 20, 27, 34, 41],
+        [12, 19, 26, 33, 40],
+        [18, 25, 32, 39],
+        [23, 28, 33, 38],
+        [17, 22, 27, 32, 37],
+        [11, 16, 21, 26, 31, 36],
+        [5, 10, 15, 20, 25, 30],
+        [4, 9, 14, 19, 24],
+        [3, 8, 13, 28],
+    ];
+
+    const getDiags = () => {
+        diags.forEach((diagIndex) => {
+            const diag = [];
+            for (let i = 0; i < diagIndex.length; i++) {
+                const currentDiagIndex = diagIndex[i];
+                diag.push(positions[currentDiagIndex]);
+            }
+            checkWinner(diag);
+        });
+    };
+
+    const getRowPositions = (rowIndex) => {
+        const row = [];
+        for (let i = 0; i < columns.length; i++) {
+            const current = columns[i];
+            const currentColomnu = current.querySelectorAll(".position");
+            const position = currentColomnu[rowIndex];
+            row.push(position);
+        }
+        return row;
+    };
+
+    const checkWinner = (positions) => {
+        let count = 0;
+        for (let i = 0; i < positions.length; i++) {
+            const current = positions[i];
+            if (current.classList.contains("player-" + currentPlayerNumber)) {
+                count++;
+                if (count === 4) {
+                    alert("player-" + currentPlayerNumber + " is the winner");
+                    location.reload();
+                }
+            } else {
+                count = 0;
+            }
+        }
+    };
+
+    const getRowIndex = (columnsPosition) => {
+        for (let i = columnsPosition.length - 1; i >= 0; i--) {
+            const current = columnsPosition[i];
+            console.log(current);
+            const isTaken =
+                current.classList.contains("player-1") ||
+                current.classList.contains("player-2");
+
+            if (!isTaken) {
+                return i;
+            }
+        }
+        console.log("-1");
+        return -1;
+    };
+
+    const switchPlayer = (playerTurn) => {
+        if (currentPlayerNumber === 1) {
+            currentPlayerNumber = 2;
+            playerTurn.innerText = "Your turn: " + currentPlayerNumber;
+        } else {
+            currentPlayerNumber = 1;
+            playerTurn.innerText = "Your turn: " + currentPlayerNumber;
+        }
+    };
+
+    for (let column of columns) {
+        column.addEventListener("click", (event) => {
+            const playerTurn = document.getElementById("player-turn");
+            const columnsPosition = event.currentTarget.querySelectorAll(
+                ".position"
+            );
+            console.log(columnsPosition);
+            const rowIndex = getRowIndex(columnsPosition);
+            columnsPosition[rowIndex].classList.add(
+                "player-" + currentPlayerNumber
+            );
+
+            checkWinner(columnsPosition);
+            const rowPosition = getRowPositions(rowIndex);
+            checkWinner(rowPosition);
+            getDiags();
+            console.log("Player ", currentPlayerNumber);
+            switchPlayer(playerTurn);
+        });
+    }
+})();
